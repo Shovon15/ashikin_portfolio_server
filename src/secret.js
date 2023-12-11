a2bffeea4da61de34ce545a65807dce5839cce73
Shovon15 <shovonrnbd@gmail.com>
2023-12-11 18:57:13 +0600

    render deploy

--- src/secret.js
+++ src/secret.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: ".env" });
 const serverPort = process.env.SERVER_PORT || 5001;
 const mongoDB = process.env.MONGODB_ATLAS_URL;
 const jwtActivationKey = process.env.JWT_ACTIVATION_KEY;
-
+//secret
 module.exports = {
 	serverPort,
 	mongoDB,
