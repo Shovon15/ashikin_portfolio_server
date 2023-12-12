3173dd0d7a31f36ee2b4b5108a489b62e351c3bb
Shovon15 <shovonrnbd@gmail.com>
2023-12-12 21:14:25 +0600

    .

--- src/app.js
+++ src/app.js
@@ -26,14 +26,19 @@ app.use(express.static("public"));
 // app.use("/public", express.static(__dirname + "/public"));
 // app.use('/public/images',express.static(__dirname + '/public/images'));
 
-app.use("/api/admin", userRouter);
-app.use("/api/events", eventRouter);
-app.use("/api/blogs", blogRouter);
-app.use("/api/seed", seedRouter);
+// app.use("/api/admin", userRouter);
+// app.use("/api/events", eventRouter);
+// app.use("/api/blogs", blogRouter);
+// app.use("/api/seed", seedRouter);
 
 app.get("/", (req, res) => {
 	res.status(200).send({
 		message: "welcome to portfolio server!!!",
+	});
+});
+app.get("/demo", (req, res) => {
+	res.status(200).send({
+		message: "demo route!!!",
 	});
 });
 
