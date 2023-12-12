af0798917a013fca1d768d59b77ba155a1389997
Shovon15 <shovonrnbd@gmail.com>
2023-12-13 00:25:05 +0600

    .

--- src/secret.js
+++ src/secret.js
@@ -1,7 +1,9 @@
 require("dotenv").config({ path: ".env" });
 
 const serverPort = process.env.SERVER_PORT || 5001;
-const mongoDB = process.env.MONGODB_URI;
+const mongoDB =
+	process.env.MONGODB_URI ||
+	"mongodb+srv://vercel-admin-user:ukMrxM8dTUrppEWc@cluster0.o5a9jb9.mongodb.net/myFirstDatabase?";
 const jwtActivationKey = process.env.JWT_ACTIVATION_KEY;
 //secret
 module.exports = {
