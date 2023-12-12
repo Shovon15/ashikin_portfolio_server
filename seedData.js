787d0c72aed28eb0422fe8f17edd65160c1fe2c5
Shovon15 <shovonrnbd@gmail.com>
2023-12-12 21:01:25 +0600

    try to deploy

--- seedData.js
+++ seedData.js
@@ -1,14 +1,8 @@
 const data = {
 	users: [
 		{
-			name:"shovon mahamud",
+			name: "shovon mahamud",
 			email: "shovonrnbd@gmail.com",
-			password: "Shovon@1",
-			isAdmin: "true",
-		},
-		{
-			name:"kamal",
-			email: "shovonkamal@gmail.com",
 			password: "Shovon@1",
 			isAdmin: "true",
 		},
--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { mongoDB } = require("../secret");
 const connectDB = async (options = {}) => {
 	try {
 		await mongoose.connect(mongoDB, options);
-		console.log("DB connect successfully!!!");
+		console.log(`DB connect successfully!!! `);
 
 		mongoose.connection.on("error", (error) => {
 			console.error("DB connection error", error);
@@ -14,4 +14,3 @@ const connectDB = async (options = {}) => {
 };
 module.exports = connectDB;
 
-// MONGODB_ATLAS_URL=mongodb+srv://shovon:m4GNxKq3yQGTy8u7@cluster0.o5a9jb9.mongodb.net/ashikinPortfolio
