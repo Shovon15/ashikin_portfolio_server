4ca13f99d044f98b509d19d03e3a9c996be6620a
Shovon15 <shovonrnbd@gmail.com>
2024-03-13 20:35:58 +0600

    file upload error 5

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,18 +1,48 @@
 const multer = require("multer");
+const fs = require("fs");
+
+// Create the destination directory if it doesn't exist
+const destinationDir = "public/temp";
+if (!fs.existsSync(destinationDir)) {
+    fs.mkdirSync(destinationDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-	destination: function (request, file, cb) {
-		cb(null, "public/temp");
-	},
-
-	//what is the file name-----------------
-	filename: function (req, file, cb) {
-		cb(null, Date.now() + "_" + file.originalname);
-	},
+    destination: function (request, file, cb) {
+        cb(null, destinationDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "_" + file.originalname);
+    },
 });
 
 const upload = multer({
-	storage,
+    storage,
 });
 
 module.exports = upload;
+
+
+
+
+
+// const multer = require("multer");
+
+// const storage = multer.diskStorage({
+// 	destination: function (request, file, cb) {
+// 		cb(null, "public/temp");
+// 	},
+
+// 	//what is the file name-----------------
+// 	filename: function (req, file, cb) {
+// 		cb(null, Date.now() + "_" + file.originalname);
+// 	},
+// });
+
+// const upload = multer({
+// 	storage,
+// });
+
+// module.exports = upload;
+
+
